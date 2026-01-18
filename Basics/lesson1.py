fc6deb1a95817ad4e773a7cca6e41f8123b95092
Fatih-Sahinoglu <2409111011@stu.istinye.edu.tr>
2026-01-18 21:59:44 +0300

    Print,comment and multi-line comment

--- Basics/lesson1.py
+++ Basics/lesson1.py
@@ -1,1 +1,8 @@
+#This is a comment
+
 print("Hello \"First Python\" Code")
+#This will print: Hello "First Python" Code
+
+"""This is a multi-line comment
+   Because this string is not assigned to any variable,
+   it will be ignored by the Python interpreter."""
